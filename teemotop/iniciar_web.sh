65646fe48c4c9164b43634decde455e4f0f5a483
Renathoo <re.lobos@duocuc.cl>
2024-05-07 21:40:40 +0000

    tarea pa irse3

--- teemotop/iniciar_web.sh
+++ teemotop/iniciar_web.sh
@@ -1,3 +1,3 @@
 docker build -t mi_teemo_web .
-docker run -t -d -p 8080:80 --name teemo_web mi_teemo_web
+docker run -t -d -p 5000:80--name teemo_web mi_teemo_web
 docker exec CONTAINER_ID apachectl start
